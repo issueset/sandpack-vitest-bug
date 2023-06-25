c61ef5acac68a9bf885b15a7fa70f43528192a18
ocavue <ocavue@gmail.com>
2023-06-25 19:58:11 +0800

    f

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,51 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import "./App.css";
+
+import { Sandpack } from "@codesandbox/sandpack-react";
+import { buildFiles } from "./template";
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <h1>With latest dependencies</h1>
+      <Sandpack
+        files={buildFiles(devDependenciesNew)}
+        customSetup={{
+          environment: "node",
+        }}
+        options={{
+          showInlineErrors: true,
+          showConsole: true,
+        }}
+      />
+
+      <h1>With older dependencies</h1>
+      <Sandpack
+        files={buildFiles(devDependenciesOld)}
+        customSetup={{
+          environment: "node",
+        }}
+        options={{
+          showInlineErrors: true,
+          showConsole: true,
+        }}
+      />
     </>
-  )
+  );
 }
 
-export default App
+const devDependenciesNew = {
+  "@vitejs/plugin-react": "4.0.1",
+  vite: "4.3.9",
+  typescript: "5.1.3",
+  "esbuild-wasm": "0.17.19",
+  // "esbuild-wasm": "0.18.8",
+};
+
+const devDependenciesOld = {
+  "@vitejs/plugin-react": "3.1.0",
+  vite: "4.1.4",
+  typescript: "4.9.5",
+  "esbuild-wasm": "0.17.19",
+};
+
+export default App;
